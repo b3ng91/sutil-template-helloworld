5049178e77400b44e842b032f81641afcd500e3f
Linus Vettiger <linus4123@gmail.com>
2021-06-17 16:20:20 +0200

    wip

--- src/App/App.fs
+++ src/App/App.fs
@@ -6,30 +6,80 @@ open Sutil.Attr
 open Feliz
 open type Feliz.length
 
+[<RequireQualifiedAccess>]
 type GridItem =
     | X
     | O
     | Empty
 
-let gridItems =
-    Array.init 9 (fun i -> (i, GridItem.Empty))
+[<RequireQualifiedAccess>]
+type Player =
+    | X
+    | O
 
-let view() =
+module Player =
+    let toGridItem player =
+        match player with
+        | Player.X -> GridItem.X
+        | Player.O -> GridItem.O
+    
+    let other player =
+        match player with
+        | Player.X -> Player.O
+        | Player.O -> Player.X
+    
+let gridItemView item =
+    match item with
+    | GridItem.X -> "X"
+    | GridItem.O -> "O"
+    | GridItem.Empty -> " "
+
+type Model = {
+    CurrentPlayer: Player
+    Grid: GridItem array
+}
+
+module Model =
+    let init () = {
+        CurrentPlayer = Player.O
+        Grid = Array.init 9 (fun _ -> GridItem.Empty)
+    }
+        
+    let updateGrid index (model: Model) =
+        let newItem = Player.toGridItem model.CurrentPlayer
+        let updateGridArray i current =
+            if i = index then
+                newItem
+            else current
+        
+        let nextPlayer = Player.other model.CurrentPlayer
+        let newGrid = Array.mapi updateGridArray model.Grid
+        { model with Grid = newGrid; CurrentPlayer = nextPlayer }
+
+let gridView (onClickHandler: int -> unit) index gridItem =
+    Html.div [
+        style [
+            Css.border (length.px 1, borderStyle.solid, color.aqua)
+            Css.padding (length.em 1)
+            Css.custom ("aspect-ratio", "1 / 1")
+        ]
+        onClick (fun _ -> onClickHandler index) [Once] 
+        text (gridItemView gridItem)
+    ]
+
+let view () =
+    let store = Store.make (Model.init ())
+    
     Html.div [
         style [
             Css.displayGrid
             Css.gridTemplateColumns [length.fr 1; length.fr 1; length.fr 1]
             Css.gridTemplateRows [length.fr 1; length.fr 1; length.fr 1]
         ]
-        for (i, gridItem) in gridItems do
-            Html.div [
-                style [
-                    Css.border (length.px 1, borderStyle.solid, color.aqua)
-                    Css.padding (length.em 1)
-                    Css.custom ("aspect-ratio", "1 / 1")
-                ]
-                text (string (1 + i))
-            ]
+        let model = Store.get store
+        yield!
+            model.Grid
+            |> Array.mapi (gridView (fun i -> Store.modify (Model.updateGrid i) store))
     ]
     
 
